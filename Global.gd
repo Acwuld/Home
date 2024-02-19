1bf7e5ab4b802049a9e0dcd606d46ed30ca5e24f
Lorente <Acwuld@outlook.com>
2024-02-19 21:33:35 +0800

    v0.0.2

--- Global.gd
+++ Global.gd
@@ -1,11 +1,30 @@
 extends Node
 
 #shape:I,J,L,O,S,T,Z
-var shapes:Dictionary={
-	"shape0":[Vector2i(0,0),Vector2i(0,1),Vector2i(0,2),Vector2i(0,3)],
-	"shape1":[Vector2i(0,0),Vector2i(0,1),Vector2i(0,2),Vector2i(-1,2)],
-	"shape2":[Vector2i(0,0),Vector2i(0,1),Vector2i(0,2),Vector2i(1,2)],
-	"shape3":[Vector2i(0,0),Vector2i(0,1),Vector2i(1,0),Vector2i(1,1)],
-	"shape4":[Vector2i(0,0),Vector2i(0,1),Vector2i(1,0),Vector2i(-1,1)],
-	"shape5":[Vector2i(0,0),Vector2i(-1,0),Vector2i(1,0),Vector2i(0,1)],
-	"shape6":[Vector2i(0,0),Vector2i(0,1),Vector2i(1,1),Vector2i(-1,0)]}
+var i0 := [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(1,3)]
+var i1 := [Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(3,1)]
+
+var j0 := [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(0,2)]
+var j1 := [Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(2,2)]
+var j2 := [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(2,0)]
+var j3 := [Vector2i(0,0), Vector2i(0,1), Vector2i(1,1), Vector2i(2,1)]
+
+var l0 := [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(2,2)]
+var l1 := [Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(2,0)]
+var l2 := [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(0,0)]
+var l3 := [Vector2i(0,2), Vector2i(0,1), Vector2i(1,1), Vector2i(2,1)]
+
+var o0 := [Vector2i(0,0), Vector2i(0,1), Vector2i(1,0), Vector2i(1,1)]
+
+var s0 := [Vector2i(1,0), Vector2i(2,0), Vector2i(0,1), Vector2i(1,1)]
+var s1 := [Vector2i(0,0), Vector2i(0,1), Vector2i(1,2), Vector2i(1,1)]
+
+var t0 := [Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(1,2)]
+var t1 := [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(2,1)]
+var t2 := [Vector2i(1,0), Vector2i(0,1), Vector2i(1,1), Vector2i(2,1)]
+var t3 := [Vector2i(1,0), Vector2i(0,1), Vector2i(1,1), Vector2i(1,2)]
+
+var z0 := [Vector2i(0,0), Vector2i(1,0), Vector2i(1,1), Vector2i(2,1)]
+var z1 := [Vector2i(2,0), Vector2i(2,1), Vector2i(1,1), Vector2i(1,2)]
+
+var shapes := [i0,j0,l0,o0,s0,t0,z0]
--- scenes/Main.gd
+++ scenes/Main.gd
@@ -5,16 +5,20 @@ var nextShape:Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	getNextShape()
+#	getNextShape()
+	nextShape = Global.j0
+	
+	drawNextShape()
+	
 #	drawNextShape()
 
 func _process(_delta):
 	pass
-func getNextShape():
-	randomize()
-	nextShapeIndex = randi_range(0,6)
-	nextShape = Global.shapes["shape"+str(nextShapeIndex)]
+#func getNextShape():
+#	randomize()
+#	nextShapeIndex = randi_range(0,6)
+#	nextShape = Global.shapes["shape"+str(nextShapeIndex)]
 
-#func drawNextShape():
-#	for cell in nextShape:
-#		$Squre.set_cell(0,cell,-1)
+func drawNextShape():
+	for cell in nextShape:
+		$Squre.set_cell(0,cell,0,Vector2i(0,0))
