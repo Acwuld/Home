f67b35212d7f86111293db198d8b0172c886ff50
Lorente <Acwuld@outlook.com>
2024-02-20 09:42:10 +0800

    v0.0.3
    add pressed key

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -1,24 +1,34 @@
 extends Node2D
 
+var currentShape:Array
+
 var nextShapeIndex:int = 1
 var nextShape:Array
-
+var gameRunning:bool
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	gameRunning=true
 #	getNextShape()
-	nextShape = Global.j0
-	
-	drawNextShape()
+	currentShape = Global.j0
+	var pos = Vector2i(0,0)
+	drawShape(currentShape,pos)
 	
 #	drawNextShape()
 
 func _process(_delta):
-	pass
+	var pos = Vector2i(0,0)
+	if gameRunning:
+		if Input.is_action_pressed("anticlockwise"):
+			eraseShape(currentShape,pos)
+			drawShape(Global.j1,pos)
 #func getNextShape():
 #	randomize()
 #	nextShapeIndex = randi_range(0,6)
 #	nextShape = Global.shapes["shape"+str(nextShapeIndex)]
 
-func drawNextShape():
-	for cell in nextShape:
-		$Squre.set_cell(0,cell,0,Vector2i(0,0))
+func drawShape(shape,pos):
+	for cell in shape:
+		$Squre.set_cell(0,cell+pos,0,Vector2i(0,0))
+func eraseShape(shape,pos):
+	for cell in shape:
+		$Squre.erase_cell(0,cell+pos)
