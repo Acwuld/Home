35a7e7e4d8028ac9cd496999bebc69f5881317c6
Lorente <Acwuld@outlook.com>
2024-02-22 10:58:18 +0800

    v0.0.4

--- Global.gd
+++ Global.gd
@@ -1,30 +1,49 @@
 extends Node
 
 #shape:I,J,L,O,S,T,Z
-var i0 := [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(1,3)]
-var i1 := [Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(3,1)]
-
-var j0 := [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(0,2)]
-var j1 := [Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(2,2)]
-var j2 := [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(2,0)]
-var j3 := [Vector2i(0,0), Vector2i(0,1), Vector2i(1,1), Vector2i(2,1)]
-
-var l0 := [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(2,2)]
-var l1 := [Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(2,0)]
-var l2 := [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(0,0)]
-var l3 := [Vector2i(0,2), Vector2i(0,1), Vector2i(1,1), Vector2i(2,1)]
-
-var o0 := [Vector2i(0,0), Vector2i(0,1), Vector2i(1,0), Vector2i(1,1)]
-
-var s0 := [Vector2i(1,0), Vector2i(2,0), Vector2i(0,1), Vector2i(1,1)]
-var s1 := [Vector2i(0,0), Vector2i(0,1), Vector2i(1,2), Vector2i(1,1)]
-
-var t0 := [Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(1,2)]
-var t1 := [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(2,1)]
-var t2 := [Vector2i(1,0), Vector2i(0,1), Vector2i(1,1), Vector2i(2,1)]
-var t3 := [Vector2i(1,0), Vector2i(0,1), Vector2i(1,1), Vector2i(1,2)]
-
-var z0 := [Vector2i(0,0), Vector2i(1,0), Vector2i(1,1), Vector2i(2,1)]
-var z1 := [Vector2i(2,0), Vector2i(2,1), Vector2i(1,1), Vector2i(1,2)]
-
-var shapes := [i0,j0,l0,o0,s0,t0,z0]
+var A:=[[Vector2i(1,1)],
+		[Vector2i(1,1)],
+		[Vector2i(1,1)],
+		[Vector2i(1,1)]]
+var B:=[[Vector2i(1,1),Vector2i(1,0)],
+		[Vector2i(1,1),Vector2i(0,1)],
+		[Vector2i(1,1),Vector2i(1,2)],
+		[Vector2i(1,1),Vector2i(2,1)]]
+var C:=[[Vector2i(1,0),Vector2i(1,1),Vector2i(1,2)],
+		[Vector2i(0,1),Vector2i(1,1),Vector2i(2,1)],
+		[Vector2i(1,0),Vector2i(1,1),Vector2i(1,2)],
+		[Vector2i(0,1),Vector2i(1,1),Vector2i(2,1)],]
+var D:=[[Vector2i(1,0),Vector2i(1,1),Vector2i(2,1)],
+		[Vector2i(0,1),Vector2i(1,1),Vector2i(1,0)],
+		[Vector2i(0,1),Vector2i(1,1),Vector2i(1,2)],
+		[Vector2i(1,2),Vector2i(1,1),Vector2i(2,1)]]
+# I
+var I:=[[Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(1,3)],
+		[Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(3,1)],
+		[Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(1,-1)],
+		[Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(-1,1)]]
+var J:=[[Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(0,2)],
+		[Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(2,2)],
+		[Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(2,0)],
+		[Vector2i(0,0), Vector2i(0,1), Vector2i(1,1), Vector2i(2,1)]]
+var L:=[[Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(2,2)],
+		[Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(2,0)],
+		[Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(0,0)],
+		[Vector2i(0,2), Vector2i(0,1), Vector2i(1,1), Vector2i(2,1)]]
+var O:=[[Vector2i(0,0), Vector2i(0,1), Vector2i(1,0), Vector2i(1,1)],
+		[Vector2i(0,2), Vector2i(0,1), Vector2i(1,1), Vector2i(1,2)],
+		[Vector2i(2,2), Vector2i(1,1), Vector2i(1,2), Vector2i(2,1)],
+		[Vector2i(2,0), Vector2i(2,1), Vector2i(1,0), Vector2i(1,1)]]
+var S:=[[Vector2i(1,0), Vector2i(2,0), Vector2i(0,1), Vector2i(1,1)],
+		[Vector2i(0,0), Vector2i(0,1), Vector2i(1,2), Vector2i(1,1)],
+		[Vector2i(1,1), Vector2i(2,1), Vector2i(0,2), Vector2i(1,2)],
+		[Vector2i(1,0), Vector2i(1,1), Vector2i(2,2), Vector2i(2,1)]]
+var T:=[[Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(1,2)],
+		[Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(2,1)],
+		[Vector2i(1,0), Vector2i(0,1), Vector2i(1,1), Vector2i(2,1)],
+		[Vector2i(1,0), Vector2i(0,1), Vector2i(1,1), Vector2i(1,2)]]
+var Z:=[[Vector2i(0,0), Vector2i(1,0), Vector2i(1,1), Vector2i(2,1)],
+		[Vector2i(1,0), Vector2i(1,1), Vector2i(0,1), Vector2i(0,2)],
+		[Vector2i(0,1), Vector2i(1,1), Vector2i(1,2), Vector2i(2,2)],
+		[Vector2i(2,0), Vector2i(2,1), Vector2i(1,1), Vector2i(1,2)]]
+var shapes := [A,B,C,D,I,J,L,O,S,T,Z]
--- scenes/Main.gd
+++ scenes/Main.gd
@@ -2,33 +2,34 @@ extends Node2D
 
 var currentShape:Array
 
-var nextShapeIndex:int = 1
+var nextShapeIndex:int = 0
+var nextType:int=0
+
 var nextShape:Array
 var gameRunning:bool
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	gameRunning=true
-#	getNextShape()
-	currentShape = Global.j0
-	var pos = Vector2i(0,0)
-	drawShape(currentShape,pos)
-	
-#	drawNextShape()
-
 func _process(_delta):
 	var pos = Vector2i(0,0)
+	currentShape = Global.shapes[nextShapeIndex][nextType]
 	if gameRunning:
-		if Input.is_action_pressed("anticlockwise"):
+		if Input.is_action_just_released("anticlockwise"):
 			eraseShape(currentShape,pos)
-			drawShape(Global.j1,pos)
-#func getNextShape():
-#	randomize()
-#	nextShapeIndex = randi_range(0,6)
-#	nextShape = Global.shapes["shape"+str(nextShapeIndex)]
-
+			nextType=(nextType+1)%4
+			currentShape = Global.shapes[nextShapeIndex][nextType]
+			drawShape(currentShape,pos)
+		if Input.is_action_just_released("hold"):
+			eraseShape(currentShape,pos)
+			nextType = 0
+			nextShapeIndex=(nextShapeIndex+1)%11
+			currentShape = Global.shapes[nextShapeIndex][nextType]
+			drawShape(currentShape,pos)
 func drawShape(shape,pos):
 	for cell in shape:
-		$Squre.set_cell(0,cell+pos,0,Vector2i(0,0))
+		if cell == Vector2i(1,1):
+			$Squre.set_cell(0,cell+pos,0,Vector2i(1,0))
+		else:
+			$Squre.set_cell(0,cell+pos,0,Vector2i(0,0))
 func eraseShape(shape,pos):
 	for cell in shape:
 		$Squre.erase_cell(0,cell+pos)
