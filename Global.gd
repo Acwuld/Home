13441dabf52ad4eec53e2a6533a26743108b80f0
Lorente <Acwuld@outlook.com>
2024-02-23 11:56:49 +0800

    v0.0.5

--- Global.gd
+++ Global.gd
@@ -17,7 +17,6 @@ var D:=[[Vector2i(1,0),Vector2i(1,1),Vector2i(2,1)],
 		[Vector2i(0,1),Vector2i(1,1),Vector2i(1,0)],
 		[Vector2i(0,1),Vector2i(1,1),Vector2i(1,2)],
 		[Vector2i(1,2),Vector2i(1,1),Vector2i(2,1)]]
-# I
 var I:=[[Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(1,3)],
 		[Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(3,1)],
 		[Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(1,-1)],
--- scenes/Main.gd
+++ scenes/Main.gd
@@ -1,35 +1,88 @@
 extends Node2D
 
-var currentShape:Array
+@onready var main: TileMap = $Main
+@onready var held: TileMap = $Held
+@onready var next: TileMap = $Next
 
-var nextShapeIndex:int = 0
-var nextType:int=0
+var currentShape:Array
+var currentShapeIndex:int
+var currentType:int=0
+var initPos:Vector2i=Vector2i(0,-13)
+var currentPos
+
+var shapes:Array = [0,1,2,3,4,5,6,7,8,9,10]
+var shapesCopy:Array = [0,1,2,3,4,5,6,7,8,9,10]
 
 var nextShape:Array
+var nextShapeIndex:int = 0
+const nextType:int=0
+
+var heldShape:Array
+var heldShapeIndex:int
+const heldType:int=0
+
 var gameRunning:bool
 func _ready():
 	gameRunning=true
+	currentShapeIndex=pickShape()
+	
+	$Timer.start()
 func _process(_delta):
-	var pos = Vector2i(0,0)
-	currentShape = Global.shapes[nextShapeIndex][nextType]
+	
+	showNext()
+	currentShape = Global.shapes[currentShapeIndex][currentType]
 	if gameRunning:
 		if Input.is_action_just_released("anticlockwise"):
-			eraseShape(currentShape,pos)
-			nextType=(nextType+1)%4
-			currentShape = Global.shapes[nextShapeIndex][nextType]
-			drawShape(currentShape,pos)
-		if Input.is_action_just_released("hold"):
-			eraseShape(currentShape,pos)
-			nextType = 0
-			nextShapeIndex=(nextShapeIndex+1)%11
-			currentShape = Global.shapes[nextShapeIndex][nextType]
-			drawShape(currentShape,pos)
-func drawShape(shape,pos):
+			eraseShape(main,currentShape,currentPos)
+			rotateShape(true)
+			drawShape(main,currentShape,currentPos)
+		#if Input.is_action_just_released("hold"):
+			#eraseShape(currentShape,pos)
+			#
+			#nextShapeIndex=(nextShapeIndex+1)%11
+			#currentShape = Global.shapes[nextShapeIndex][nextType]
+			#drawShape(currentShape,pos)
+func pickShape():
+	currentPos = initPos
+	var shape=0
+	if not shapesCopy.is_empty():
+		shapesCopy.shuffle()
+		print(shapesCopy)
+		shape = shapesCopy.pop_back()
+		print(shapesCopy)
+	else:
+		shapesCopy = shapes.duplicate()
+		shapesCopy.shuffle()
+		shape = shapesCopy.pop_back()
+	return shape
+func drawShape(tile,shape,pos):
 	for cell in shape:
 		if cell == Vector2i(1,1):
-			$Squre.set_cell(0,cell+pos,0,Vector2i(1,0))
+			tile.set_cell(0,cell+pos,0,Vector2i(1,0))
 		else:
-			$Squre.set_cell(0,cell+pos,0,Vector2i(0,0))
-func eraseShape(shape,pos):
+			tile.set_cell(0,cell+pos,0,Vector2i(0,0))
+func eraseShape(tile,shape,pos):
 	for cell in shape:
-		$Squre.erase_cell(0,cell+pos)
+		tile.erase_cell(0,cell+pos)
+func rotateShape(direction:bool):
+	if direction:
+		currentType=(currentType+1)%4
+		currentShape = Global.shapes[currentShapeIndex][currentType]
+	else:
+		currentType=(currentType+3)%4
+		currentShape = Global.shapes[currentShapeIndex][currentType]
+func holdShape():
+	pass
+func moveShape():
+	pass
+func showNext():
+	nextShapeIndex = shapesCopy[-1]
+	drawShape(next,Global.shapes[nextShapeIndex][0],Vector2i(0,0))
+	pass
+func moveDown():
+	eraseShape(main,currentShape,currentPos)
+	currentPos+=Vector2i.DOWN
+	drawShape(main,currentShape,currentPos)
+
+func _on_timer_timeout():
+	moveDown() # Replace with function body.
